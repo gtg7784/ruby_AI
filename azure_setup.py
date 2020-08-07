f38167a27cf16c8ebf3aed7928f5f7c8e52e8d9e
gtg7784 <gtg7784@naver.com>
2020-08-07 22:41:52 +0900

    add azure ml setup python

--- azure_setup.py
+++ azure_setup.py
@@ -3,9 +3,9 @@ from azureml.core import Workspace
 
 print(azureml.core.VERSION)
 
-subscription_id = '<subscription-id>'
-resource_group  = '<resource-group>'
-workspace_name  = '<workspace-name>'
+subscription_id = 'd294ecf5-c9bb-49ce-8ad5-9707f6504612'
+resource_group  = 'ruby_resource'
+workspace_name  = 'ruby_workspace'
 
 try:
     ws = Workspace(subscription_id = subscription_id, resource_group = resource_group, workspace_name = workspace_name)
